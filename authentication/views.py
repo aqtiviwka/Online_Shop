6aac462ab661dd281f3fa91f0eec55a6ff55ef44
Dzmitry <qkoowka@gmail.com>
2022-12-08 00:49:04 +0300

    finished register, login templates and added views and urls

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 
+
+def login_user(request):
+    return render(request, 'auth/login.html')
+
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
 # Create your views here.
